08d57e8d91acff681ea315e6b55b2d50c8720078
Marco <marcobaruco2091@gmail.com>
2025-07-02 19:39:59 -0300

    first commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { LucideAngularModule, Heart } from 'lucide-angular';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  readonly Heart = Heart;
 }
